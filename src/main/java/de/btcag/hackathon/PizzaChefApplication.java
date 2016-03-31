29b1bc011822083e3b4be4ea4df96038feecd754
Florian Postel <florian.postel@btc-ag.com>
2016-03-31 21:28:33 +0200

    +spring initializr

---
+++ src/main/java/de/btcag/hackathon/PizzaChefApplication.java
@@ -0,0 +1,12 @@
+package de.btcag.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaChefApplication.class, args);
+	}
+}
